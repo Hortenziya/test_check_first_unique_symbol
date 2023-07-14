c7be17f56016bad02143907a6800cd1fdcae117e
anastasiaceban <nastcheban@gmail.com>
2023-07-14 16:40:33 +0300

    add: - choose and add to the list first unique letter from a word - return first unique letter from the list

---
+++ main.py
@@ -0,0 +1,38 @@
+
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
+
+
+@app.route('/process_form', methods=['POST'])
+def upload_file():
+    text = request.form['text_input'].replace('\r\n', ' ').split()
+    lists_of_unique_symbol = get_unique_symbol_string(text)
+    return get_unique_symbol(lists_of_unique_symbol)
+
+
+def get_unique_symbol_string(user_input):
+    lists_of_unique_symbol = []
+    for string in user_input:
+        for symbol in string:
+            if symbol.isalpha() and list(string).count(symbol) == 1:
+                lists_of_unique_symbol.append(symbol)
+                break
+    return lists_of_unique_symbol
+
+
+def get_unique_symbol(lists_of_symbol):
+    for each in lists_of_symbol:
+        if lists_of_symbol.count(each) == 1:
+            return f'unique: {each}'
+
+
+if __name__ == '__main__':
+    app.run(port=6001)
+
+
